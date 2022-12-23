a4a240eb38070f7aa630f89492b7d9716971cbdc
WaRaN1 <bezckorovayniy1988@gmail.com>
2022-12-23 04:50:54 +0200

    Bot

--- Bot_main_file.py
+++ Bot_main_file.py
@@ -15,6 +15,12 @@ button_registration = types.InlineKeyboardButton("Реєстрація")
 button_authorization = types.InlineKeyboardButton("Авторизація")
 free_access.add(button_registration, button_authorization)
 
+main_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+button_work_with_a_cash = types.InlineKeyboardButton("Робота з рахунком")
+button_purchase_of_goods = types.InlineKeyboardButton("Купівля товарів")
+button_training = types.InlineKeyboardButton("Тренування")
+main_keyboard.add(button_work_with_a_cash, button_purchase_of_goods, button_training)
+
 ivan = telebot.TeleBot(config["token"])
 
 
@@ -24,65 +30,71 @@ def start(message):
                       "Вітаємо у системі спортзалу, якщо ви є нашим клієнтом, то пройдіть авторизацію, "
                       "а якщо ні, то ласкаво просимо на реєстрацію", reply_markup=free_access)
 
+
 @ivan.message_handler(content_types=["text"])
 def get_text(message):
-    now_time = time.time()
-
-    file = open(clients, "r", encoding='utf-8')
-    now_users = file.read().split("\n")
-    file.close()
-
+    # now_time = time.time()
+    # file = open(clients, "r", encoding='utf-8')
+    # user_time_all = file.read().split("\n")
+    # user_time_all = user_time_all[0:len(user_time_all)-1]
+    # file.close()
     if message.text.lower() == "авторизація":
-        ivan.register_next_step_handler(ivan.send_message(message.chat.id, "Введіть ваше ім'я та пароль через '/' для "
-                                                                           "вхoду у систему чатбота"), authorization)
+        ivan.register_next_step_handler(ivan.send_message(message.chat.id, "Введіть пароль для входу"),
+                                        authorization)
     elif message.text.lower() == "реєстрація":
-        ivan.register_next_step_handler(ivan.send_message(message.chat.id, "Введіть ваше ім'я та пароль через "
-                                                                           "'/'"), registration)
+        ivan.register_next_step_handler(ivan.send_message(message.chat.id, "Придумайте пароль (не менше шести "
+                                                                           "символів)"), registration)
 
 
-def registration(message):                                  # Реєстрація у системі
-    file = open(clients, "r", encoding='utf-8')
-    all_users = file.read().split("\n")
-    file.close()
-    rez = message.text.split("/")
-    var_var = 0
-    for el in all_users:                                    # Перевірка чи такий користувач вже зареєстрований
-        var = el.split("/")
-        if var[0] == rez[0]:
-            var_var = 1
-            break
-    if var_var == 0:
-        nowtime = time.time()
-        file = open(clients, "a", encoding='utf-8')
-        file.write(f"{message.text}/{nowtime}\n")
-        file.close()
-        ivan.send_message(message.chat.id, f"Вітаю {rez[0]}, вас успішно зареєстровано у системі ")
+
+def registration(message):
+    if len(message.text) < 6:  # Перевіряємо корректність введеного паролю
+        ivan.send_message(message.chat.id, "Некорректно введений пароль, повторіть процедуру реєстрації")
     else:
-        ivan.send_message(message.chat.id, f"Щось пішло не так. Користувач {rez[0]} вже зареєстрований у системі")
+        file = open(clients, "r", encoding='utf-8')
+        all_users = file.read().split("\n")
+        file.close()
+        clients_n, all_users_now = "-", ""
+        for elem in range(len(all_users)):
+            if message.chat.id == int(
+                    all_users[elem].split("/")[0]):  # Перевіряємо чи даний клієнт вже був зареєстрований раніше
+                print("Yes")
+                all_users[elem] = f"{all_users[elem].split('/')[0]}/{message.text}/{time.time()}/" \
+                                  f"{all_users[elem].split('/')[3]}"  # Якщо клієнт є в базі, то просто змінюємо пароль
+                for el in all_users:  # Підготовлюємо бд клієнтів для перезапису у файл
+                    all_users_now += f"{el}\n"
+                file = open(clients, "w", encoding='utf-8')
+                file.write(all_users_now[0:len(all_users_now) - 1])
+                file.close()
+                clients_n = "+"
+                break
+        if clients_n == "-":  # Якщо клієнта не знайдено у базі, то йго дані просто туди дозаписуємо
+            file = open(clients, "a", encoding='utf-8')
+            file.write(f"{message.chat.id}/{message.text}/{time.time()}/0\n")
+            file.close()
+        ivan.send_message(message.chat.id, f"Вітаю, вас успішно зареєстровано у системі, отримайте вашу карту та "
+                                           f"авторизуйтесь для входу")
 
 
 def authorization(message):
-    file = open(path, "r", encoding='utf-8')
+    file = open(clients, "r", encoding='utf-8')
     log_pass = file.read().split("\n")
-    rez = text.split("/")
     var = 0
-    for ind in range(len(log_pass)+1):
-        log_pass[ind] = log_pass[ind].split('/')
-        if rez[0] == log_pass[ind][0] and rez[1] == log_pass[ind][1]:
-            ivan.send_message(message.chat.id, f"{rez[0]}, вітаємо у системі", reply_markup=keyboard)
-            global nameUser
-            nameUser = f"{rez[0]}"
-            var, old = 1, time.time()
-            log_pass[ind] = f'{log_pass[ind][0]}/{log_pass[ind][1]}/{message.chat.id}/{old}'
-            var_var = ''
-            print(log_pass)
-            for ind in range(len(log_pass)):
-                var_var += f'{log_pass[ind]}\n'
-            file = open(path, "w", encoding='utf-8')
-            file.write(var_var)
-            file.close()
-            break
+    for ind in range(len(log_pass)):
+        if len(log_pass[ind]) > 1:
+            if str(log_pass[ind].split("/")[1]) == str(message.text) and message.chat.id == int(
+                    log_pass[ind].split("/")[0]):
+                ivan.send_message(message.chat.id, "Вітаємо у системі", reply_markup=main_keyboard)
+                log_pass[ind] = f"{message.chat.id}/{message.text}/{time.time()}/{log_pass[ind].split('/')[3]}"
+                var_var, var = '', 1
+                for ind in range(len(log_pass)):
+                    var_var += f'{log_pass[ind]}\n'
+                file = open(clients, "w", encoding='utf-8')
+                file.write(var_var[0:len(var_var) - 1])
+                file.close()
+                break
     if var == 0:
-        ivan.send_message(message.chat.id, f"Невірно введені логін або пароль.")
+        ivan.send_message(message.chat.id, f"Невірно введений пароль, або ви не зареєстровані у системиі")
+
 
 ivan.polling(none_stop=True, interval=0)
